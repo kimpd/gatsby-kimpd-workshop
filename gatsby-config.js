5328383a360a10d45237e77b55f041184a2d8e47
KimPD <kimpd@hotmail.com>
2022-02-01 12:45:24 +0900

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-
-    ]
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 function IndexPage() {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="2007 Japan, Sibuya."
+        src="../images/2007-11-15-travel.jpg"
+      />
     </Layout>
   )
 }
